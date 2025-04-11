1b1332cfc57f12a00028d587b9e584ad79e8551a
LENOVO <LENOVO@DESKTOP-NP6QSFQ>
2025-04-11 12:15:59 +0700

    Add project files.

---
+++ PR_SistemManajemenKaryawan/Karyawan.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_SistemManajemenKaryawan
+{
+    class Karyawan
+    {
+        private string nama;
+        private string id;
+        private double gajiPokok;
+
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+
+        public string ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public double GajiPokok
+        {
+            get { return gajiPokok; }
+            set { gajiPokok = value; }
+        }
+
+        // Method virtual HitungGaji
+        public virtual double HitungGaji()
+        {
+            return GajiPokok;
+        }
+
+        
+        public void TampilkanData()
+        {
+            Console.WriteLine("Nama       : " + Nama);
+            Console.WriteLine("ID         : " + ID);
+            Console.WriteLine("Gaji Pokok : Rp" + GajiPokok);
+            Console.WriteLine("Gaji Akhir : Rp" + HitungGaji());
+        }
+    }
+
+    class KaryawanTetap : Karyawan
+    {
+        public override double HitungGaji()
+        {
+            return GajiPokok + 500000;
+        }
+    }
+
+    class KaryawanKontrak : Karyawan
+    {
+        public override double HitungGaji()
+        {
+            return GajiPokok - 200000;
+        }
+    }
+
+    class KaryawanMagang : Karyawan
+    {
+        public override double HitungGaji()
+        {
+            return GajiPokok;
+        }
+    }
+
+}
---
+++ PR_SistemManajemenKaryawan/program.cs
@@ -0,0 +1,45 @@
+﻿using PR_SistemManajemenKaryawan;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Jenis Karyawan (Tetap/Kontrak/Magang): ");
+        string jenis = Console.ReadLine().ToLower();
+
+        Console.Write("Nama: ");
+        string nama = Console.ReadLine();
+
+        Console.Write("ID: ");
+        string id = Console.ReadLine();
+
+        double gaji;
+        Console.Write("Gaji Pokok: ");
+        while (!double.TryParse(Console.ReadLine(), out gaji))
+        {
+            Console.Write("Masukkan angka yang valid untuk gaji: ");
+        }
+
+        //objek berdasarkan jenis karyawan
+        Karyawan karyawan;
+        if (jenis == "tetap")
+        {
+            karyawan = new KaryawanTetap();
+        }
+        else if (jenis == "kontrak")
+        {
+            karyawan = new KaryawanKontrak();
+        }
+        else
+        {
+            karyawan = new KaryawanMagang();
+        }
+
+        karyawan.Nama = nama;
+        karyawan.ID = id;
+        karyawan.GajiPokok = gaji;
+        karyawan.TampilkanData();
+        Console.ReadLine(); 
+    }
+}
